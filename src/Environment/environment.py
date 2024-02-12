c6fdd91a73207058058fafc17a67f240e90be363
RojanRegmi <rjnrcks@gmail.com>
2024-02-12 10:32:42 +0100

    Environment Changes

--- src/Environment/environment.py
+++ src/Environment/environment.py
@@ -5,15 +5,11 @@ from typing import List, Optional, Tuple
 
 from tf_agents.agents import tf_agent
 from tf_agents.drivers import driver
-from tf_agents.environments import py_environment
-from tf_agents.environments import tf_environment
-from tf_agents.environments import bandit_py_environment
+from tf_agents.environments import py_environment, tf_environment
 from tf_agents.policies import tf_policy
 from tf_agents.specs import array_spec
 from tf_agents.specs import tensor_spec
 from tf_agents.trajectories import time_step as ts
-from tf_agents.trajectories import trajectory
-from tf_agents.trajectories import policy_step
 from tf_agents.typing import types
 
 import pickle
@@ -72,21 +68,24 @@ class BanditPyEnvironment(py_environment.PyEnvironment):
 
 class MyModelSelectionEnv(BanditPyEnvironment):
 
-    def __init__(self, time_series_dir: str, list_thresholds: List[float], list_gtruth: List[float]):
+    def __init__(self, time_series: pd.DataFrame, list_thresholds: List[float], list_gtruth: List[float]):
 
-        super().__int__(list_thresholds, list_gtruth)
+        super().__init__(list_thresholds, list_gtruth)
 
-        self.time_series = pd.read_csv(time_series_dir)
-        self.subsequences = data_process(time_series_dir)
+        self.time_series = time_series
+        self.subsequences = data_process(time_series)
 
         self.list_thresholds = list_thresholds
+        self.gtruth = list_gtruth
+
+        self.len_data = len(time_series)
 
         self.features_obj = FeatureExtractor()
 
         self.action_list = []
 
-        action_spec = array_spec.BoundedArraySpec(shape=(2,), dtype=np.int32, minimum=0, maximum = 1, name='Select Models')
-        observation_spec = array_spec.BoundedArraySpec(shape=(1, 318), dtype=np.float64, name='observation')
+        action_spec = array_spec.BoundedArraySpec(shape=(), dtype=np.int32, minimum=0, maximum = 1, name='Select Models')
+        observation_spec = array_spec.BoundedArraySpec(shape=(1, 159), dtype=np.float64, name='observation') # shape changed from (1, 318) to (1, 159)
 
         super(MyModelSelectionEnv, self).__init__(observation_spec, action_spec)
 
@@ -114,56 +113,70 @@ class MyModelSelectionEnv(BanditPyEnvironment):
             self.done = True
         else:
             self.done = False
-        
-        return ts.termination(self._observe(), reward)
+
+        if not self.done:
+            return ts.transition(self._observe(), reward)
+            #return ts.transition(self.subsequences[self.pointer], reward)
+        else:
+           return ts.termination(self._observe(), reward)
+           #return ts.termination(self.subsequences[self.pointer], reward)
 
                 
     def _observe(self):
 
-        current_subsequence = self.subsequences[self.pointer]
-        self._observation = self._feature_extractor(current_subsequence)
+        self._observation = self._feature_extractor(self.subsequences[self.pointer])
         return self._observation
     
     
     def _apply_action(self, action):
 
         if action == 0:
-            model = pickle.load(open(f'../../saved_models/iforest_dodgers_v2.sav','rb'))
-            feats = self.subsequences[self.pointer]
+            model = pickle.load(open(f'../saved_models/iforest_dodgers_v2.sav','rb'))
+            feats = self.subsequences[self.pointer].reshape(-1,1)
             score = model.decision_function(feats)
 
-            if score < self.list_thresholds[0]:
-                label = 1
-            else:
-                label = 0
+            label_list = [1 if i < self.list_thresholds[0] else 0 for i in score]
+            label_np = np.array(label_list)
 
         elif action == 1:
-            model = pickle.load(open(f'../../saved_models/osvm_dodgers_v2.sav', 'rb'))
-            feats = self._observe()
+            model = pickle.load(open(f'../saved_models/osvm_dodgers_v2.sav', 'rb'))
+            feats = self.subsequences[self.pointer].reshape(-1,1)
             score = model.decision_function(feats)
 
-            if score > self.list_thresholds[1]:
-                label = 1
-            else:
-                label = 0
+            label_list = [1 if i > self.list_thresholds[1] else 0 for i in score]
+            label_np = np.array(label_list)
 
-        reward = self._reward_function(label)
+        reward = self._reward_function(label_np)
 
         self.action_list.append(action)
         
         return reward
     
-    def _reward_function(self, label):
+    def _reward_function(self, label_np):
 
-        if self.gtruth[self.pointer]==1: # If the ground truth is 1 anomaly
-            if label==1: # If the model predicts 1 anomaly correctly - True Positive (TP)
-                reward = 1
-            else: # If the model predicts 0 normal incorrectly - False Negative (FN)
-                reward = -1.5
-        else: # If the ground truth is 0 normal
-            if label==1: # If the model predicts 1 anomaly incorrectly - False Positive (FP)
-                reward = -0.5
-            else: # If the model predicts 0 normal correctly - True Negative (TN)
-                reward = 0.1
+        gtruth_split = self.gtruth[self.pointer:self.pointer+50]
+        gtruth_np = np.array(gtruth_split)
+
+        tp = 0
+        fn = 0
+        fp = 0
+        tn = 0
+
+        for i, j in zip(gtruth_split, label_np):
+           
+           if i==1 and j==1:   # If the model predicts anomaly correctly - True Positive (TP)
+              tp +=1
+              
+           elif i==1 and j==0: # If the model predicts 0 normal incorrectly - False Negative (FN)
+              fn +=1
+        
+           elif i==0 and j==1: # If the model predicts 1 anomaly incorrectly - False Positive (FP)
+              fp +=1
+        
+           elif i ==0 and j==0: # If the model predicts 0 normal correctly - True Negative (TN)
+              tn +=1
+
+        
+        reward = (1 * tp + (-1.5) * fn + (-0.5) * fp + 0.1 * tn) / len(gtruth_split)
 
         return reward
